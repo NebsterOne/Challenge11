321052f1448aa86351b1f94a05d5dc656e489581
N Z <nelectrik@gmail.com>
2023-01-14 23:16:55 +1030

    apiRoutes.js added

---
+++ routes/apiRoutes.js
@@ -0,0 +1,38 @@
+const note = require("express").Router();
+const { readAndAppend } = require("../helpers/fsUtils");
+const uuid = require("../helpers/uuid");
+
+// GET Route for retrieving all the notes
+note.get("/notes", (req, res) =>
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+);
+
+// POST Route for submitting notes
+note.post("/notes", (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+
+      text_id: uuid(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting Note");
+  }
+});
+
+module.exports = note;
